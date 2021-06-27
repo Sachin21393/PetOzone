6a1f26ab36715884c5db22e1cf8ef28d3205e0e0
Sachin21393 <76869950+Sachin21393@users.noreply.github.com>
2021-06-28 00:00:18 +0530

    Update admin_doc.js

--- static/js/admin_doc.js
+++ static/js/admin_doc.js
@@ -4,3 +4,28 @@ function sendUpdateRequest(){
     const number = document.getElementById("number").value;
     fetch(`add_to_database_doctor/${key}/${name}/${number}`)
 }  
+
+function htmlEncode(value) {
+ return $('<div/>').text(value)
+ .html();
+ }
+
+ $(function () {
+
+ // Specify an onclick function
+ // for the generate button
+ $('#generate').click(function () {
+
+ // Generate the link that would be
+ // used to generate the QR Code
+ // with the given data
+ let finalURL =
+'https://chart.googleapis.com/chart?cht=qr&chl=' +
+ htmlEncode($('#content').val()) +
+ '&chs=160x160&chld=L|0'
+
+ // Replace the src of the image with
+ // the QR code image
+ $('.qr-code').attr('src', finalURL);
+ });
+ });
